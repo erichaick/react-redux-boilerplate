e834cbd884825fa0d7aa3f78e9e21d778009b25c
erichaick <erichai@gmail.com>
2019-08-25 21:30:23 +0800

    init import

---
+++ src/configureStore.js
@@ -0,0 +1,27 @@
+import { createBrowserHistory } from "history";
+import { applyMiddleware, compose, createStore } from "redux";
+import { routerMiddleware } from "connected-react-router";
+import createRootReducer from "./reducers";
+import thunk from "redux-thunk";
+
+export const history = createBrowserHistory();
+const middlewares = [
+  routerMiddleware(history), // for dispatching history actions
+  thunk
+];
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+export default function configureStore(preloadedState) {
+  const store = createStore(
+    createRootReducer(history), // root reducer with router state
+    preloadedState,
+    composeEnhancers(applyMiddleware(...middlewares))
+  );
+
+  return store;
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { Route, Switch } from "react-router";
+import { ConnectedRouter } from "connected-react-router";
+import configureStore, { history } from "./configureStore";
+import * as serviceWorker from "./serviceWorker";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore(/* provide initial state if any */);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route exact path="/" render={() => <App />} />
+      </Switch>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
